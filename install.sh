66902c997eead28656ea1060556cf05ed0a6edef
James Cartledge <jcartledge@gmail.com>
2014-06-28 20:32:31 +1000

    handle spaces in filenames ffs

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ shopt -s nullglob
 
 function install {
   PLATFORM=$1
-  for file in $(find $PLATFORM/); do
+  find $PLATFORM/ | while read file; do
 
     DIR=$PWD/$PLATFORM
-    if ! [[ -f $file ]];      then continue; fi
-    if [[ $file =~ .git/ ]];  then continue; fi
+    if ! [[ -f "$file" ]];      then continue; fi
+    if [[ "$file" =~ .git/ ]];  then continue; fi
 
     versioned=$($READLINK -f "$file")
     echo "Checking $versioned"
@@ -21,30 +21,30 @@ function install {
     original=${versioned/#$DIR/~}
     canonical_original=$($READLINK -m "$original")
     if [ -h "$original" -a ! -e "$canonical_original" ]; then
-      rm $original
+      rm "$original"
       canonical_original=$original
     fi
 
-    if [[ $versioned == $canonical_original ]]; then
+    if [[ "$versioned" == "$canonical_original" ]]; then
       echo "Symlink already installed."
       echo
       continue
     fi
 
-    if [[ -f $canonical_original ]]; then
+    if [[ -f "$canonical_original" ]]; then
       # should show a diff and give the option of bailing
       backup="$canonical_original.$NOW.bak"
       echo "Moving $canonical_original to $backup"
-      $(mv $canonical_original $backup)
+      $(mv "$canonical_original" "$backup")
     fi
 
-    dir=$(dirname $canonical_original)
-    if [[ ! -d $dir ]]; then
-      $(mkdir -p $dir)
+    dir=$(dirname "$canonical_original")
+    if [[ ! -d "$dir" ]]; then
+      $(mkdir -p "$dir")
     fi
 
     echo "Linking $canonical_original to $versioned"
-    $(ln -s $versioned $canonical_original)
+    $(ln -s "$versioned" "$canonical_original")
     echo
 
   done
